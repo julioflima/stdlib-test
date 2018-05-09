5a49ebe24fe0b7709dd135fb7352725fe17a170c
juloko <julio_flima@hotmail.com>
2018-05-09 14:55:18 -0300

    done

--- main.c
+++ main.c
@@ -92,7 +92,10 @@ void main()
 }
 
 void testStdlib(uint8_t option){
-    char buffer[50];
+    char buffer[70];
+    for(char i = 0 ; buffer[0] != '\0' ; i++){
+        buffer[i] = "";
+    }
     switch(option){
     case 1:
         USARTWriteLine("You enter in the test of abs():");        
@@ -116,86 +119,156 @@ void testStdlib(uint8_t option){
         USARTWriteLine("You enter in the test of fabs():");        
         USARTWriteLine("Returns the absolute value of as float x.");
         USARTWriteLine("If we do fabs(-45645.36545), the return it's gonna be: ");
-        USARTWriteLine("sprintf(buffer, ""%ld"", fabs(-147483648));");      
+        USARTWriteLine("sprintf(buffer, ""%ld"", fabs(-45645.36545));");      
         USARTWriteLine("/opt/microchip/xc8/v1.45/sources/common/fabs.c:4: error: (1098) conflicting declarations for variable ""_fabs"" (main.c:120)");
         USARTWriteLine("Not implemented in stdlib.");
-
         USARTGotoNewLine();
     break;
     
     case 4:
         USARTWriteLine("You enter in the test of ftoa():");
-        USARTWriteLine("sfdsfd");
+        USARTWriteLine("Converts an integer value to a null-terminated string using the specified base and stores the result in the array given by str parameter.");
+        USARTWriteLine("If we pass the number 3.1415, the return it's gonna be: ");        
+        USARTWriteLine("In decimal: ");
+        //ftoa (3.1,buffer);
+        USARTWriteLine(buffer);
+        USARTWriteLine("Not working");
         USARTGotoNewLine();
     break;
     
     case 5:
         USARTWriteLine("You enter in the test of itoa():");
-        USARTWriteLine("sfdsfd");
+        USARTWriteLine("Converts an integer value to a null-terminated string using the specified base and stores the result in the array given by str parameter.");
+        USARTWriteLine("If we pass the number 654, the return it's gonna be: ");        
+        itoa (buffer,654,10);
+        USARTWriteLine("In decimal: ");
+        USARTWriteLine(buffer);
+        itoa (buffer,654,16);
+        USARTWriteLine("In hexadecimal: ");
+        USARTWriteLine(buffer);
+        itoa (buffer,654,2);
+        USARTWriteLine("In binary: ");
+        USARTWriteLine(buffer);
         USARTGotoNewLine();
     break;
     
     case 6:
         USARTWriteLine("You enter in the test of ltoa():");
-        USARTWriteLine("sfdsfd");
+        USARTWriteLine("Converts an long integer value to a null-terminated string using the specified base and stores the result in the array given by str parameter.");
+        USARTWriteLine("If we pass the number -483648, the return it's gonna be: ");        
+        ltoa (buffer,-483648,10);
+        USARTWriteLine("In decimal: ");
+        USARTWriteLine(buffer);
+        ltoa (buffer,-483648,16);
+        USARTWriteLine("In hexadecimal: ");
+        USARTWriteLine(buffer);
+        ltoa (buffer,-483648,2);
+        USARTWriteLine("In binary: ");
+        USARTWriteLine(buffer);
         USARTGotoNewLine();
     break;
     
     case 7:
         USARTWriteLine("You enter in the test of utoa():");
-        USARTWriteLine("sfdsfd");
+        USARTWriteLine("Converts an long integer value to a null-terminated string using the specified base and stores the result in the array given by str parameter.");
+        USARTWriteLine("If we pass the number 483648, the return it's gonna be: ");        
+        USARTWriteLine("In decimal: ");
+        utoa (buffer,483648,10);
+        USARTWriteLine("In decimal: ");
+        USARTWriteLine(buffer);
+        utoa (buffer,483648,16);
+        USARTWriteLine("In hexadecimal: ");
+        USARTWriteLine(buffer);
+        utoa (buffer,483648,2);
+        USARTWriteLine("In binary: ");
+        USARTWriteLine(buffer);
+        USARTWriteLine("Not working");
         USARTGotoNewLine();
     break;
     
     case 8:
-        USARTWriteLine("You enter in the test of atof():");
-        USARTWriteLine("sfdsfd");
-
-
+        USARTWriteLine("You enter in the test of atoi():");
+        USARTWriteLine("The string pointed to by the argument str is converted to an integer (type int).");
+        USARTWriteLine("If the number cannot be converted, then 0 is returned.");
+        USARTWriteLine("If we pass the number 2, the return it's gonna be: ");
+        *buffer = "2.15";
+        int value = atoi (buffer);
+        char buffer_2[10];
+        itoa (buffer_2,value,16);
+        USARTWriteLine("In decimal: ");
+        USARTWriteLine(buffer_2);
         USARTGotoNewLine();
+        USARTWriteLine("Not working");
     break;
     
     case 9:
         USARTWriteLine("You enter in the test of atoi():");
         USARTWriteLine("The string pointed to by the argument str is converted to an integer (type int).");
         USARTWriteLine("If the number cannot be converted, then 0 is returned.");
-
+        USARTWriteLine("If we pass the number 2, the return it's gonna be: ");
+        *buffer = "2";
+        int value = atoi (buffer);
+        char buffer_2[10];
+        itoa (buffer_2,value,16);
+        USARTWriteLine("In decimal: ");
+        USARTWriteLine(buffer_2);
         USARTGotoNewLine();
+        USARTWriteLine("Not working");
     break;
     
     case 10:
         USARTWriteLine("You enter in the test of atol():");
         USARTWriteLine("The string pointed to by the argument str is converted to an integer (type long).");
         USARTWriteLine("If the number cannot be converted, then 0 is returned.");
+        USARTWriteLine("If we pass the number 2, the return it's gonna be: ");
+        *buffer = "2";
+        int value = atol (buffer);
+        char buffer_2[10];
+        itoa (buffer_2,value,16);
+        USARTWriteLine("In decimal: ");
+        USARTWriteLine(buffer_2);
+        USARTWriteLine("Not working");
         USARTGotoNewLine();
     break;
     
     case 11:
         USARTWriteLine("You enter in the test of strtol():");
         USARTWriteLine("The string pointed to by the argument str is converted to an integer (type long).");        
-        USARTWriteLine("This value according to the given base, which must be between 2 and 36 inclusive, or be the special value 0.");
-
-        
-
-
-
-
+        USARTWriteLine("If the number cannot be converted, then 0 is returned.");
+        USARTWriteLine("If we pass the number ""2261465 this is a test"", the return it's gonna be: ");
+        *buffer = "2261465 this is a test";
+        char *ptr;
+        long value2;
+        value2 = strtol (buffer, &ptr, 10);
+        ltoa (buffer_2,value2,10);
+        USARTWriteLine("In decimal: ");
+        USARTWriteLine(buffer_2);
+        USARTWriteLine("In string: ");
+        USARTWriteLine(ptr);
+        USARTWriteLine("Not working");
         USARTGotoNewLine();
     break;
     
     case 12:
-        USARTWriteLine("You enter in the test of xtoi():");
-        USARTWriteLine("sfdsfd");
-
+          USARTWriteLine("You enter in the test of xtoi():");
+        USARTWriteLine("The string pointed to by the argument str is converted to an integer (type long).");
+        USARTWriteLine("If the number cannot be converted, then 0 is returned.");
+        USARTWriteLine("If we pass the number 2, the return it's gonna be: ");
+        *buffer = "2";
+        int value = xtoi (buffer);
+        char buffer_2[10];
+        itoa (buffer_2,value,16);
+        USARTWriteLine("In decimal: ");
+        USARTWriteLine(buffer_2);
+        USARTWriteLine("Not working");
         USARTGotoNewLine();
     break;
     
     case 13:
         USARTWriteLine("You enter in the test of rand():");
         USARTWriteLine("The C library function int rand(void) returns a pseudo-random number in the range of 0 to RAND_MAX.");
-        sprintf(buffer, "%i", rand());
-        USARTWriteLine("Not working.");
-        USARTGotoNewLine();
+        ltoa (buffer,rand(),10);
+        USARTWriteLine(buffer);
     break;
     
     case 14:
@@ -203,8 +276,8 @@ void testStdlib(uint8_t option){
         USARTWriteLine("The C library function void srand(unsigned int seed) seeds the random number generator used by the function rand.");
         USARTWriteLine("If we seed them with 152, the return it's gonna be: ");
         srand((unsigned) 152);
-        sprintf(buffer, "%ld", rand());
-        USARTWriteLine("Not working.");
+        ltoa (buffer,rand(),10);
+        USARTWriteLine(buffer);
         USARTGotoNewLine();
     break;
     
